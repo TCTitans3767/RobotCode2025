6fe1c857a2fb92edefb56f8c177a9dde3fb56482
ElectricE-123 <156331888+ElectricE-123@users.noreply.github.com>
2025-02-01 11:21:50 -0500

    Finished base code for climber

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,6 +24,23 @@ public class Constants {
 
     }
 
+    public final class Climber {
+        public final static int leftMotorID = 0;
+        public final static int rightMotorID = 1;
+
+        public final static double kP = 0.1;
+        public final static double kI = 0.1;
+        public final static double kD = 0.1;
+        public final static double kG = 0.1;
+        public final static double kV = 0.1;
+        public final static double kS = 0.1;
+
+        public final static double maxVelocity = 0.1;
+        public final static double maxAcceleration = 0.1;
+
+        public final static double conversonFactor = 1;
+    }
+
     public final class Robot {
         public final static double chassisWidth = 26.0;
         public final static double chassisDepth = 26.0;
