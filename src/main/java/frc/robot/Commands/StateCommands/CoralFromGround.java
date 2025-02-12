3fd534bb8fca0cbf85de8f04fd31778eeb17fabb
Th3F4nd1t <indephisive@gmail.com>
2025-02-12 15:49:42 -0800

    Coral from ground

--- src/main/java/frc/robot/Commands/StateCommands/CoralFromGround.java
+++ src/main/java/frc/robot/Commands/StateCommands/CoralFromGround.java
@@ -1,7 +1,25 @@
 package frc.robot.Commands.StateCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.Commands.GeneralControlCommands.SetArmAngle;
+import frc.robot.Commands.GeneralControlCommands.SetElevatorPosition;
 
-public class CoralFromGround extends Command{
+public class CoralFromGround extends SequentialCommandGroup{
     
+    public CoralFromGround() {
+        addCommands(
+            new ParallelCommandGroup(
+                new SetElevatorPosition(0),
+                new SetArmAngle(0)//,
+                // new SetIntakePosition(),
+                // new SetClimbPosition(),
+                // new SetManipulatorSpeed(),
+                // new SetDrivertrainTarget()
+            ),
+            new WaitUntilCommand(() -> true)
+        );
+    }
 }
