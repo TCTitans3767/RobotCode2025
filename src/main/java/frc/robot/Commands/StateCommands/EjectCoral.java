74717ee0873e9048813b4e599403e0062909d240
ElectricE-123 <156331888+ElectricE-123@users.noreply.github.com>
2025-02-12 19:07:43 -0500

    Even more stuff done
    Eject coral

---
+++ src/main/java/frc/robot/Commands/StateCommands/EjectCoral.java
@@ -0,0 +1,24 @@
+package frc.robot.Commands.StateCommands;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.Commands.GeneralControlCommands.SetArmAngle;
+import frc.robot.Commands.GeneralControlCommands.SetElevatorPosition;
+
+public class EjectCoral extends SequentialCommandGroup{
+    
+    public EjectCoral() {
+        addCommands(
+            new ParallelCommandGroup(
+                new SetElevatorPosition(0),
+                new SetArmAngle(0)//,
+                // new SetIntakePosition(),
+                // new SetClimbPosition(),
+                // new SetManipulatorSpeed(),
+                // new SetDrivertrainTarget()
+            ),
+            new WaitUntilCommand(() -> true)
+        );
+    }
+}
