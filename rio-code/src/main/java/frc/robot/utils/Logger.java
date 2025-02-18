645021e2693f34f69a9af03806fcc1f30c55fee3
Kevin Killingsworth <kevin@redfenix.com>
2025-02-17 19:49:44 -0500

    Update Arm Subsystem
    - Added specific logger methods
    - Only log when actually moving the arm
    - Name units in rotations instead of angle = rotations
    - Small naming fixes

--- rio-code/src/main/java/frc/robot/utils/Logger.java
+++ rio-code/src/main/java/frc/robot/utils/Logger.java
@@ -2,8 +2,32 @@ package frc.robot.utils;
 
 import dev.doglog.DogLog;
 
-public class Logger extends DogLog{
-    
-    
+public class Logger extends DogLog {
+    public static void logLimitError(String message) {
+        DogLog.log("Error/Limit", message);
+    }
 
+    public static void logArmSetPosition(double rotations) {
+        DogLog.log("Arm/Set Position", rotations);
+    }
+
+    public static void logArmAbsolute(double degrees) {
+        DogLog.log("Arm/Absolute Degrees", degrees);
+    }
+
+    public static void logArmMotorPosition(double position) {
+        DogLog.log("Arm/Motor Position", position);
+    }
+
+    public static void logArmMotorSpeed(double speed) {
+        DogLog.log("Arm/Motor Speed", speed);
+    }
+
+    public static void logArmMotorVelocity(double velocity) {
+        DogLog.log("Arm/Motor Velocity", velocity);
+    }
+
+    public static void logArmAtPosition(boolean isAtPosition) {
+        DogLog.log("Arm/at position", isAtPosition);
+    }
 }
