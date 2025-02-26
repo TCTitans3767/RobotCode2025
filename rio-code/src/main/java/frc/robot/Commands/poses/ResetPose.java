ef5ad2d32fe06ec5082d757c013a802d47d3aa18
jckitkat <136133147+jckitkat@users.noreply.github.com>
2025-02-26 08:35:26 -0500

    added ResetPose command
    this will later look different from initialTransitPose

--- rio-code/src/main/java/frc/robot/Commands/poses/ResetPose.java
+++ rio-code/src/main/java/frc/robot/Commands/poses/ResetPose.java
@@ -1,11 +1,26 @@
 package frc.robot.Commands.poses;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Robot;
+import frc.robot.Commands.arm.SetArmAngle;
+import frc.robot.Commands.elevator.SetElevatorPosition;
+import frc.robot.subsystems.RobotMode;
 
 public class ResetPose extends SequentialCommandGroup{
     
     public ResetPose() {
-        
+
+        addCommands(
+            // new SetIntakePosition(0),
+            new SetArmAngle(-0.3),
+            new SetElevatorPosition(0.5),
+            new SetArmAngle(0.128),
+            new InstantCommand(() -> {Robot.robotMode.setDriveModeCommand(RobotMode.controllerDrive);}).ignoringDisable(true),
+            new InstantCommand(() -> {Robot.robotMode.setCurrentMode(RobotMode.transit);}).ignoringDisable(true)
+        );
+
+        addRequirements(Robot.arm, Robot.climber, Robot.intake, Robot.manipulator, Robot.elevator);
     }
 
 }
