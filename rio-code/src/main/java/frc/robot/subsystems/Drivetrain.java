c4c344e84bcfb4f31a4372eae6a1ff792e6aa3d7
jckitkat <136133147+jckitkat@users.noreply.github.com>
2025-02-28 09:26:32 -0500

    added wheel diameter calculation command from system meltdowns 2024 codebase

--- rio-code/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ rio-code/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -341,4 +341,12 @@ public class Drivetrain extends TunerSwerveDrivetrain implements Subsystem {
         });
         m_simNotifier.startPeriodic(kSimLoopPeriod);
     }
+
+    public double getYaw() {
+        return getPigeon2().getYaw().getValueAsDouble();
+    }
+
+    public void driveRobotRelative(double xVelocity, double yVelocity, double rotationVelocity) {
+        setControl(new SwerveRequest.RobotCentric().withVelocityX(xVelocity).withVelocityY(yVelocity).withRotationalRate(rotationVelocity));
+    }
 }
