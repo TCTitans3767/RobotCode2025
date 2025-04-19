51bc1b3c1a09d4af7f773428c0ade246683143c2
Jacob Mertz <jcmertz@outlook.com>
2025-04-18 23:12:41 -0500

    end of worlds day 3

--- rio-code/src/main/java/frc/robot/Commands/modes/L1.java
+++ rio-code/src/main/java/frc/robot/Commands/modes/L1.java
@@ -5,15 +5,19 @@ import frc.robot.Robot;
 import frc.robot.TriggerBoard;
 import frc.robot.subsystems.RobotMode;
 
-public class L1 extends Command{
-    
+public class L1 extends Command {
+
     public L1() {
         addRequirements(Robot.arm, Robot.climber, Robot.intake, Robot.manipulator, Robot.elevator);
     }
 
     @Override
     public void execute() {
-        
+
+        if (TriggerBoard.isCoralInManipulator()) {
+            Robot.robotMode.setCurrentMode(RobotMode.transitPose);
+        }
+
         if (TriggerBoard.isCoralButtonPressed()) {
             Robot.robotMode.setCurrentMode(RobotMode.scoreCoralPose);
         }
